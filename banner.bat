0443b63ecf47347f5f4183fb4b4e5a9fd61f7970
Inorizushi <inorizushi@gmail.com>
2016-06-11 01:22:36 -0700

    Heavily Impove batch files. No longer allows users to constantly delete their files, everything can now be separated into game folders.
    Look at the code for more details.

--- banner.bat
+++ banner.bat
@@ -1,11 +1,10 @@
 @echo off
-if not exist input\label.png echo WARNING: Missing label image.
-if not exist input\banner.png echo WARNING: Missing banner image.
-set title=blargSNES
-set release=2014
 set f=14
 set k=1
 set ln=1
+
+:begin
+cls
 echo VIRTUAL CONSOLE INFO:
 set /p title=Game Title: 
 set /p release=Release Year: 
@@ -13,24 +12,91 @@ echo VIRTUAL CONSOLE INFO STYLE:
 set /p f=Font Size: 
 set /p k=Letter Spacing: 
 set /p ln=Number of Lines: 
-tools\convert input\label.png -rotate 270 -resize 23x44! output\label.png
-tools\convert tools\USA_EN3.png output\label.png -geometry +122+205 -composite output\USA_EN3.png
-tools\convert input\label.png -resize 54x18! output\label.png
-tools\convert tools\EUR_EN3.png output\label.png -geometry +198+227 -composite output\EUR_EN3.png
+
+if not exist input\%title% (
+		CALL :bad
+) else (
+		call :exportfull
+)
+
+:bad
+echo WARNING: Missing:
+if not exist input\%title%\label.png echo   -"%title%\label.png"
+if not exist input\%title%\banner.png echo   -"%title%\banner.png"
+set /p choice1="Do you want to use blargSNES' default graphics? (Y/N): " %=%
+if [%choice1%]==[Y] goto defaults
+if [%choice1%]==[y] goto defaults
+if [%choice1%]==[N] goto customname
+if [%choice1%]==[n] goto customname
+
+:customname
+set /p choice2="Do you want to use the data you typed? (Y/N): " %=%
+if [%choice2%]==[Y] goto exportdef
+if [%choice2%]==[y] goto exportdef
+if [%choice2%]==[N] goto exit
+if [%choice2%]==[n] goto exit
+
+:defaults
+set title=blargSNES
+set release=2014
+goto exportdef
+
+:exportdef
+cd /d %~dp0
+
+mkdir "output\%title%\"
+set lt=3
+set lr=6
+if %ln% EQU 2 ( set lt=1
+set lr=20 )
+tools\convert tools\USA_EN2.png -gravity center -font input\SCE-PS3-RD-R-LATIN.TTF -pointsize %f% -kerning %k% -fill #1e1e1e -interword-spacing 6 -interline-spacing %lt% -annotate +44+0 "%title%\n" -pointsize 14 -kerning 1.5 -interline-spacing %lr% -annotate +46+0 "\nReleased: %release%" output\%title%\USA_EN2.png
+goto end
+
+:exportfull
+cd /d %~dp0
+
+mkdir "output\%title%\"
+tools\convert input\%title%\label.png -rotate 90 -resize 23x44! output\%title%\label.png
+tools\convert tools\USA_EN3.png output\%title%\label.png -geometry +122+205 -composite output\%title%\USA_EN3.png
+tools\convert input\%title%\label.png -resize 54x18! output\label.png
+tools\convert tools\EUR_EN3.png output\%title%\label.png -geometry +198+227 -composite output\%title%\EUR_EN3.png
 del output\label.png
-tools\convert input\banner.png -resize 42x42! output\tempicon.png
-tools\convert tools\icon.png output\tempicon.png -gravity center -composite output\icon.png
+tools\convert input\%title%\banner.png -resize 42x42! output\tempicon.png
+tools\convert tools\icon.png output\tempicon.png -gravity center -composite output\%title%\icon.png
 del output\tempicon.png
-tools\convert input\banner.png -resize 120x102! output\tempbanner.png
-tools\convert tools\COMMON1.png output\tempbanner.png -geometry +4+6 -composite output\common1.png
-tools\convert input\banner.png -resize 60x51! output\tempbanner.png
-tools\convert tools\COMMON1_2.png output\tempbanner.png -geometry +2+3 -composite output\common1_2.png
-tools\convert input\banner.png -resize 30x26! output\tempbanner.png
-tools\convert tools\COMMON1_3.png output\tempbanner.png -geometry +1+1 -composite output\common1_3.png
+tools\convert input\%title%\banner.png -resize 120x102! output\tempbanner.png
+tools\convert tools\COMMON1.png output\tempbanner.png -geometry +4+6 -composite output\%title%\common1.png
+tools\convert input\%title%\banner.png -resize 60x51! output\tempbanner.png
+tools\convert tools\COMMON1_2.png output\tempbanner.png -geometry +2+3 -composite output\%title%\common1_2.png
+tools\convert input\%title%\banner.png -resize 30x26! output\tempbanner.png
+tools\convert tools\COMMON1_3.png output\tempbanner.png -geometry +1+1 -composite output\%title%\common1_3.png
 del output\tempbanner.png
 set lt=3
 set lr=6
 if %ln% EQU 2 ( set lt=1
 set lr=20 )
-tools\convert tools\USA_EN2.png -gravity center -font input\SCE-PS3-RD-R-LATIN.TTF -pointsize %f% -kerning %k% -interword-spacing 6 -interline-spacing %lt% -annotate +44+0 "%title%\n" -pointsize 14 -kerning 1.5 -interline-spacing %lr% -annotate +46+0 "\nReleased: %release%" output\USA_EN2.png
-echo Done!
+tools\convert tools\USA_EN2.png -gravity center -font input\SCE-PS3-RD-R-LATIN.TTF -pointsize %f% -kerning %k% -fill #1e1e1e -interword-spacing 6 -interline-spacing %lt% -annotate +44+0 "%title%\n" -pointsize 14 -kerning 1.5 -interline-spacing %lr% -annotate +46+0 "\nReleased: %release%" output\%title%\USA_EN2.png
+
+cd /d %~dp0\banner
+copy banner0.bcmdl banner0.bak
+set /p choicerg="What region are you building for? (1-13): "  %=% 
+copy banner%choicerg%.bcmdl banner%choicerg%.bak
+cd ..
+goto end
+
+:end
+cd "tools"
+
+set /p OpenOha="Do you want to open Ohana3DS? (Y/N): "  %=%
+if [%OpenOha%]==[Y] start Ohana3DS.exe
+if [%OpenOha%]==[y] start Ohana3DS.exe
+if [%OpenOha%]==[N] goto exit
+if [%OpenOha%]==[n] goto exit
+
+:exit
+cd ..
+set /p choice3="Do you want to restart? (Y/N): "  %=%
+if [%choice3%]==[Y] goto begin
+if [%choice3%]==[y] goto begin
+if [%choice3%]==[N] exit
+if [%choice3%]==[n] exit
--- make.bat
+++ make.bat
@@ -1,20 +1,33 @@
 @echo off
 echo blargSNES -- Virtual Console Edition
-if not exist romfs\rom.smc (
+set /p title=Game Title: 
+if not exist romfs\%title%.smc (
     echo ERROR: Missing rom file.
     echo Make sure you have a rom.smc file on the romfs folder.
     pause
     exit
 )
-set /p title=Game Title: 
 set /p author=Publisher: 
 set /p serial=Product Code: 
 set /p id=Unique Id: 
-tools\bannertool makesmdh -s "%title%" -l "%title%" -p "%author%" -i "output\icon.png" -o "icon.bin"
+set /p region=Region:
+cd romfs
+rename %title%.smc rom.smc
+cd ..
+tools\bannertool makesmdh -s "%title%" -l "%title%" -p "%author%" -i "output\%title%\icon.png" -o "icon.bin"
 tools\3dstool -c -f banner.bin -t banner --banner-dir banner
 echo %title%> romfs\rom.txt
 tools\makerom -f cia -target t -rsf "tools\custom.rsf" -o "cia\%title%.cia" -exefslogo -icon "icon.bin" -banner "banner.bin" -elf "tools\blargSnes.elf" -DAPP_TITLE="%title%" -DAPP_PRODUCT_CODE="%serial%" -DAPP_UNIQUE_ID=0x%id% -DAPP_ROMFS="romfs"
 del banner.bin
 del icon.bin
 del romfs\rom.txt
+cd romfs
+rename rom.smc %title%.smc
+cd ..
+cd banner
+del banner0.bcmdl
+rename banner0.bak banner0.bcmdl
+del banner%region%.bcmdl
+rename banner%region%.bak banner%region%.bcmdl
+cd ..
 echo Done
